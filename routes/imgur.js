405ea88cec17a81c480a43249399b4d561000e30
isdaft <ben.ashby2@gmail.com>
2016-01-15 00:07:37 +0000

    incomplete, but started func for time, and insertion of search string into mongodb

--- routes/imgur.js
+++ routes/imgur.js
@@ -2,7 +2,7 @@
 // these hold the mappings between short codes and longUrls
 var valid = false;
 var imgurk = require('imgurk');
-
+var count = 0;
 
 
 function mongoInsert(db, collection_name, data,cb) {
@@ -19,8 +19,24 @@ function mongoInsert(db, collection_name, data,cb) {
         }); //insert end
     } //mongoInsert func end
 
+function zero(n){ return n < 10 ? '0' + n : n }
 
+
+function timeStamp() {
+    var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var hours = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	var returnString = year + "-" +zero(month) + "-" + zero(day) + "T" + zero(hours) + ":" + zero(min) + ":" + sec ;
+	return returnString;
+}
+
+function insertSearchAt(searchstring, time){
     
+}    
 
 
 exports.getPath = function (req, res){
@@ -30,6 +46,15 @@ exports.getPath = function (req, res){
     var offset = req.param('offset');
     var obj = {};
     var linx = [];
+    var lastcount = count; //@ this pt in time
+    
+    count++;
+    if (lastcount < count){
+        //here we find current date & time and insert insert into mongod to be able to pulled later
+        var time = timeStamp();
+        insertSearchAt(searchString, time);
+    }
+    
     //res.send(searchString);
     imgurk.img('search', searchString, 'af9edeb4a606629', function(link){
         //res.send(link); //link[0].id
@@ -43,7 +68,7 @@ exports.getPath = function (req, res){
             linx.push(obj);
         }
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(linx));
+        res.send(JSON.stringify(linx) + " " + count);
     });
     
     
